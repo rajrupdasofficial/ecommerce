ff7733e2c364ace8947e18cff506f358e1b44979
RajrupDasid <rajrupofficialid@protonmail.com>
2024-04-29 11:27:23 +0530

    some models hasbeen registerd through admin for checking the application is working or not
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import (Products, TopBanner, Promotebanner,
+                     Category, Subcategory, ProductSize, CustomerProfile, Cart, OrderPlaced, OrderHistory)
 
-# Register your models here.
+
+@admin.register(TopBanner)
+class TopBannerAdmin(admin.ModelAdmin):
+    exclude = ["uid",]
+    list_display = ("description",)
+    list_per_page = 50
+
+
+@admin.register(Products)
+class ProductsAdmin(admin.ModelAdmin):
+    exclude = ["uid",]
+    list_per_page = 50
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ["uid",]
+    list_per_page = 50
+
+
+@admin.register(Subcategory)
+class SubCategoryAdmin(admin.ModelAdmin):
+    exclude = ["uid",]
+    list_per_page = 50
