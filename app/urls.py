8e11c55109cc7395cc186efa1c97f00648c3f1e9
RajrupDasid <rajrupofficialid@protonmail.com>
2024-05-02 20:26:09 +0530

    auth urls and other urls file
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import index, renderproductdetailspage
-
+from .views import index, renderproductdetailspage, CustomerRegistrationView, logout_view
+from django.contrib.auth import views as auth_views
+from .forms import LoginForm
 
 urlpatterns = [
     path("", index, name='index'),
     path("<str:slug>/", renderproductdetailspage, name="detailview"),
+    path("accounts/registration/", CustomerRegistrationView.as_view(),
+         name='customerregistration'),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='app/login.html',
+         authentication_form=LoginForm), name='login'),
+    path('accounts/logout/', logout_view, name='logout')
 ]
